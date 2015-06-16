ed450ace4e30827900651e23ab8321b229ed9d52
yoon <yoon@yoon-PC>
2015-06-16 17:06:38 +0900

    a

--- web02/src/main/java/net/bitacademy/spring/dao/BoardDao.java
+++ web02/src/main/java/net/bitacademy/spring/dao/BoardDao.java
@@ -1,144 +1,78 @@
 package net.bitacademy.spring.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
+import java.io.InputStream;
 import java.util.List;
 
 import net.bitacademy.spring.vo.Board;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 public class BoardDao {
-  public List<Board> selectList() throws Exception{
-    Connection con = null;
-    Statement stmt = null;
-    ResultSet rs = null;
-    
+  static SqlSessionFactory sqlSessionFactory;
+  static{
     try{
-      Class.forName("com.mysql.jdbc.Driver");
-      con = DriverManager.getConnection(
-          "jdbc:mysql://localhost:3306/studydb", "study", "study"); 
-      
-      stmt = con.createStatement();
-      rs = stmt.executeQuery("select bno, title, cre_dt, views"
-          + " from board"
-          + " order by bno desc");
-      
-      ArrayList<Board> boards = new ArrayList<Board>();
-      Board board = null;
-      while (rs.next()) {
-        board = new Board();
-        board.setNo(rs.getInt("bno"));
-        board.setTitle(rs.getString("title"));
-        board.setCreateDate(rs.getDate("cre_dt"));
-        board.setViews(rs.getInt("views"));
-        boards.add(board);
-    }
-      return boards;
-      
+      InputStream inputStream = Resources.getResourceAsStream(
+          "net/bitacademy/spring/dao/mybatis-config.xml");
+      sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }catch(Exception e){
+      e.printStackTrace();
+    }    
+  }
+  
+  public List<Board> selectList() throws Exception{
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    try{
+      return sqlSession.selectList("net.bitacademy.spring.dao.BoardDao.selectList");
     }finally{
-      try{rs.close();}catch(Exception ex){}
-      try{stmt.close();}catch(Exception ex){}
-      try{con.close();}catch(Exception ex){}
+      sqlSession.close();
     }
   }
   
   
   public Board selectOne(int no) throws Exception{
-    Connection con = null;
-    Statement stmt = null;
-    ResultSet rs = null;
+    SqlSession sqlSession = sqlSessionFactory.openSession();
     
     try{
-      Class.forName("com.mysql.jdbc.Driver");
-      con = DriverManager.getConnection(
-          "jdbc:mysql://localhost:3306/studydb", "study", "study"); 
-      stmt = con.createStatement();
-      rs = stmt.executeQuery(
-          "select bno, title, content, cre_dt, views"
-          + " from board"
-          + " where bno=" + no);
-      
-      if(!rs.next()){
-        throw new Exception("해당 번호의 게시물이 존재하지 않습니다.");
-      }
-      
-      Board board = null;
-      board = new Board();
-      board.setNo(rs.getInt("bno"));
-      board.setTitle(rs.getString("title"));
-      board.setContent(rs.getString("content"));
-      board.setCreateDate(rs.getDate("cre_dt"));
-      board.setViews(rs.getInt("views"));
-      
-      return board;
+      return sqlSession.selectOne("net.bitacademy.spring.dao.BoardDao.selectOne", no);
     } finally{
-      try{rs.close();}catch(Exception ex){}
-      try{stmt.close();}catch(Exception ex){}
-      try{con.close();}catch(Exception ex){}
+      sqlSession.close();
     }
    }
   
   public int insert(Board board) throws Exception{
-    Connection con = null;
-    PreparedStatement stmt = null;
-    
+    SqlSession sqlSession = sqlSessionFactory.openSession();
     try{
-      Class.forName("com.mysql.jdbc.Driver");
-      con = DriverManager.getConnection(
-          "jdbc:mysql://localhost:3306/studydb", "study", "study"); 
-      stmt = con.prepareStatement(
-          "insert into board(title, content, cre_dt) values(?,?,now())");
-      stmt.setString(1, board.getTitle());
-      stmt.setString(2, board.getContent());
-      return stmt.executeUpdate();
-       
-    }finally{
-      try{stmt.close();}catch(Exception ex){}
-      try{con.close();}catch(Exception ex){}
+      int count =  sqlSession.insert("net.bitacademy.spring.dao.BoardDao.insert", board);
+      sqlSession.commit();
+      return count;
+    } finally{
+      sqlSession.close();
     }
   }
   
-  
  public int update(Board board) throws Exception{
-   Connection con = null;
-   PreparedStatement stmt = null;
-   
+   SqlSession sqlSession = sqlSessionFactory.openSession();
    try{
-     Class.forName("com.mysql.jdbc.Driver");
-     con = DriverManager.getConnection(
-         "jdbc:mysql://localhost:3306/studydb", "study", "study"); 
-     stmt = con.prepareStatement("update board set title=?, content=? where bno =?");
-     stmt.setString(1, board.getTitle());
-     stmt.setString(2, board.getContent());
-     stmt.setInt(3, board.getNo());
-     
-     return  stmt.executeUpdate(); 
-  }finally{
-     try{stmt.close();}catch(Exception ex){}
-     try{con.close();}catch(Exception ex){}
+     int count = sqlSession.update("net.bitacademy.spring.dao.BoardDao.update", board);
+     sqlSession.commit();
+     return count;
+   } finally{
+     sqlSession.close();
    }
  } 
   
  
  public int delete(int no) throws Exception{
-   Connection con = null;
-   PreparedStatement stmt = null;
-   
+   SqlSession sqlSession = sqlSessionFactory.openSession();
    try{
-     Class.forName("com.mysql.jdbc.Driver");
-     con = DriverManager.getConnection(
-         "jdbc:mysql://localhost:3306/studydb", "study", "study"); 
-     stmt = con.prepareStatement("delete  from board where bno=?");
-     stmt.setInt(1, no);
-     
-     return stmt.executeUpdate(); 
-      
-   }finally{
-     try{stmt.close();}catch(Exception ex){}
-     try{con.close();}catch(Exception ex){}
+     int count = sqlSession.delete("net.bitacademy.spring.dao.BoardDao.delete", no);
+     sqlSession.commit();
+     return count;
+   } finally{
+     sqlSession.close();
    }
  }
 }
